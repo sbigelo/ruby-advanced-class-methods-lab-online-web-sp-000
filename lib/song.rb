1dabd0d5d79b214e3945e8f8f835d2f501911901
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-19 21:50:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,44 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(sname)
+    song = self.new
+    song.name = sname
+    song
+ 
+  end
+
+  def self.create_by_name(sname)
+    song = self.new
+    song.save
+    song.name = sname
+    song
+  
+  end
+
+  def self.find_by_name(sname)
+    @@all.find{|person| person.name == sname}
+    
+    
+  end
+
+  def self.find_or_create_by_name(sname)
+    self.find_by_name(sname) || self.create_by_name(sname)
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |a| a.name }
+  end
+  
+  def self.new_from_filename(filename)
+    filename = self.create
+    self.all.gsub(/[.mp3]/, )
+  end
+"Ruby".gsub!(/[tyru]/, '<\1>')   
 end
